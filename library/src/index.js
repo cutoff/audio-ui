d3030a77ee748a29938dfff933ad49b3942e0a58
Renaud Denis <mail@renauddenis.com>
2024-04-09 16:01:31 +0200

    Cutoff Library: index file and package configuration update

---
+++ library/src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+
+export { default as KnobSwitch } from './components/KnobSwitch';
+export { default as Button } from './components/Button';
+export { default as Slider } from './components/Slider';
+export { default as Keyboard } from './components/Keyboard';
+export { default as Option } from './components/Option';
+export { default as Knob } from './components/Knob';
