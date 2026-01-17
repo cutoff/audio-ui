bca499dd41d945435ffabc03495aa52b3cd9694e
Renaud Denis <mail@renauddenis.com>
2026-01-17 17:30:19 +0100

    Move the annoying NextJS dev indicator where it doesn't interfere with the content of the app

--- apps/playground-react/next.config.ts
+++ apps/playground-react/next.config.ts
@@ -12,6 +12,9 @@ const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true, // Ignore ESLint errors during production builds
     },
+    devIndicators: {
+        position: "bottom-right",
+    },
     webpack: (config) => {
         config.resolve.alias = {
             ...config.resolve.alias,
